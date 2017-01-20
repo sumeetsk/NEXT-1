b9a7bc0e281b3db655f36dc8ce00379a76cf7f3a
jain <jain@jains-MacBook-Pro.local>
2017-01-20 18:56:39 -0500

    Cleaned up test_api file

--- apps/ActiveRanking/tests/test_api.py
+++ apps/ActiveRanking/tests/test_api.py
@@ -1,21 +1,19 @@
-import numpy
-import numpy as np
-import numpy.random
-import random
-import json
-import time
-from datetime import datetime
-import requests
-from scipy.linalg import norm
-import time
 from multiprocessing import Pool
 import os
 import sys
 from joblib import Parallel, delayed
+
+import json
+import time, datetime
+import requests
+
+import numpy as np
+from scipy.linalg import norm
+
 try:
     import next.apps.test_utils as test_utils
 except:
-    sys.path.append('../../../next/apps/')
+    sys.path.append('../../next/apps/')
     import test_utils
 
 
@@ -31,9 +29,7 @@ def test_api(assert_200=True, num_arms=100, num_clients=30, delta=0.05,
              total_pulls_per_client=20, num_experiments=1,
              params={'num_tries': 5}):
 
-    app_id = 'ActiveRanking'
-    true_means = numpy.array(range(num_arms)[::-1])/float(num_arms)
-    true_means = np.arange(num_arms)
+
     pool = Pool(processes=num_clients)
     supported_alg_ids = ['AR_Random', 'Quicksort', 'ValidationSampling']
 
@@ -46,37 +42,22 @@ def test_api(assert_200=True, num_arms=100, num_clients=30, delta=0.05,
         alg_item['alg_label'] = alg_id+'_'+str(i)
         alg_list.append(alg_item)
 
-    #params = []
-    #for algorithm in alg_list:
-    #    params.append({'alg_label': algorithm['alg_label'], 'proportion':1./len(alg_list)})
     params = [{'alg_label': 'AR_Random', 'proportion': 9./28},
-        {'alg_label': 'Quicksort', 'proportion': 14./28},
-        {'alg_label': 'ValidationSampling', 'proportion': 5./28}]
-    algorithm_management_settings = {}
-    #algorithm_management_settings['mode'] = 'fixed_proportions'
-    algorithm_management_settings['mode'] = 'custom'
-    algorithm_management_settings['params'] = params
-
-    print algorithm_management_settings
-
+              {'alg_label': 'Quicksort', 'proportion': 14./28},
+              {'alg_label': 'ValidationSampling', 'proportion': 5./28}]
+    algorithm_management_settings = {'mode':custom, 'params':params}
     #################################################
     # Test POST Experiment
     #################################################
-    initExp_args_dict = {}
+    initExp_args_dict = {'app_id' : 'ActiveRanking'}
     initExp_args_dict['args'] = {'alg_list': alg_list,
                                  'algorithm_management_settings': algorithm_management_settings,
                                  'context': 'Which place looks safer?',
                                  'context_type': 'text',
-                                 'debrief': 'Test debried.',
-                                 #'failure_probability': 0.05,
+                                 'debrief': 'Test debrief.',
                                  'instructions': 'Test instructions.',
                                  'participant_to_algorithm_management': 'one_to_many',
                                  'targets': {'n': num_arms}}
-
-    initExp_args_dict['app_id'] = app_id
-    initExp_args_dict['site_id'] = 'replace this with working site id'
-    initExp_args_dict['site_key'] = 'replace this with working site key'
-
     exp_info = []
     for ell in range(num_experiments):
         print 'launching experiment'
@@ -84,27 +65,21 @@ def test_api(assert_200=True, num_arms=100, num_clients=30, delta=0.05,
         print 'done launching'
 
     # Generate participants
-    participants = []
-    pool_args = []
-    for i in range(num_clients):
-        participant_uid = '%030x' % random.randrange(16**30)
-        participants.append(participant_uid)
-
-        experiment = numpy.random.choice(exp_info)
-        exp_uid = experiment['exp_uid']
-        pool_args.append((exp_uid, participant_uid, total_pulls_per_client,
-                          true_means,assert_200))
+    participants = ['%030x' % random.randrange(16**30) for i in range(num_clients)]
+    pool_args = [(numpy.random.choice(exp_info)['exp_uid'],
+                  participant_uid,
+                  total_pulls_per_client,
+                  true_means,assert_200) for participant_uid in participants]
+    true_means = numpy.array(range(num_arms)[::-1])/float(num_arms)
+    true_means = np.arange(num_arms)
 
     print 'starting to simulate all the clients...'
     results = pool.map(simulate_one_client, pool_args)
-    # results = Parallel(n_jobs=num_clients, backend='threading')(
-    #             (delayed(simulate_one_client, check_pickle=False)(a) for a in pool_args))
     print 'done simulating clients'
 
     for result in results:
         result
 
-    #test_utils.getModel(exp_uid, app_id, supported_alg_ids, alg_list)
 
 def simulate_one_client(input_args):
     exp_uid,participant_uid,total_pulls,true_means,assert_200 = input_args
@@ -115,16 +90,11 @@ def simulate_one_client(input_args):
         print "        Participant {} is pulling {}/{} arms: ".format(participant_uid, t, total_pulls)
 
         # test POST getQuery #
-        # return a widget 1/5 of the time (normally, use HTML)
-        widget = random.choice([True] + 4*[False])
-        getQuery_args_dict = {'args': {'participant_uid': participant_uid,
-                                       'widget': widget},
+        getQuery_args_dict = {'args': {'participant_uid': participant_uid, 'widget': False},
                               'exp_uid': exp_uid}
         query_dict, dt = test_utils.getQuery(getQuery_args_dict)
         getQuery_times.append(dt)
 
-        if widget:
-            query_dict = query_dict['args']
         query_uid = query_dict['query_uid']
         targets = query_dict['target_indices']
 
@@ -132,9 +102,7 @@ def simulate_one_client(input_args):
         right = targets[1]['target']
 
         np.random.seed()
-        asd = np.random.random()
-        print 'asd=',asd
-        if asd<1./10:
+        if np.random.random()<1./10:
             f = open('Drops.log', 'a')
             f.write(str(query_dict)+'\n\n')
             f.close()
@@ -162,15 +130,6 @@ def simulate_one_client(input_args):
         processAnswer_times += [dt]
 
 
-    #r = test_utils.format_times(getQuery_times, processAnswer_times, total_pulls,
-    #               participant_uid)
-    #r = test_utils.format_times(getQuery_times, processAnswer_times, len(getQuery_times),
-    #               participant_uid)
-    #print(r)
-    #return r
-
 
 if __name__ == '__main__':
     test_api()
-    #test_api(assert_200=True, num_arms=5, num_clients=10, delta=0.05,
-                #    total_pulls_per_client=10, num_experiments=1)
