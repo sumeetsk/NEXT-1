d6f869723e3a0405402234380e3cec21b4bc8998
Sumeet <sumeetsk@gmail.com>
2017-02-28 14:25:53 -0600

    Added ALG-EVALUATION to AR_Random

--- apps/ActiveRanking/algs/AR_Random/AR_Random.py
+++ apps/ActiveRanking/algs/AR_Random/AR_Random.py
@@ -15,9 +15,8 @@ class AR_Random:
     app_id = 'ActiveRanking'
 
     def initExp(self, butler, n=None, params=None):
-        W = numpy.zeros((n,n))
         butler.algorithms.set(key='n', value=n)
-        butler.algorithms.set(key='W', value=W.tolist())
+        self.log(butler, 'initExp', [])
         return True
 
     
@@ -27,65 +26,25 @@ class AR_Random:
 
         index = np.random.randint(n)
         alt_index = np.random.randint(n)
+        query = [index, alt_index]
         while alt_index == index:
             alt_index = np.random.randint(n)
 
         utils.debug_print('Current Query ' + str([index, alt_index]))
         utils.debug_print('End of AR_Random getQuery')
-        butler.log('Events', {'exp_uid': butler.exp_uid,
-                              'alg':'AR', 'function':'getQuery',
-                              'left_id':index, 'right_id':alt_index, 'winner_id':'None', 'id':'None',
-                              'timestamp':utils.datetimeNow(),
-                              'waitingforresponse':None,
-                              'stackparametersallqs':None,
-                              'arrlist':None,
-                              'participant':participant_uid,
-                              'msg':'Success'})
+        self.log(butler, 'getQuery', query)
         return [index, alt_index, 0]
 
     def processAnswer(self, butler, left_id=0, right_id=0, winner_id=0, quicksort_data=0):
         utils.debug_print('In AR_Random processAnswer '+str([left_id, right_id, winner_id, quicksort_data]))
-
-        W = np.array(butler.algorithms.get(key='W'))
-        butler.log('Queries', {'exp_uid': butler.exp_uid,
-                               'alg':'AR',
-                               'left_id':left_id, 'right_id':right_id, 'winner_id':winner_id, 'data':0,
-                               'timestamp':utils.datetimeNow()})
-
-        if left_id == winner_id:
-            W[left_id, right_id] = W[left_id, right_id] + 1
-        else:
-            W[right_id, left_id] = W[right_id, left_id] + 1
-
-        butler.algorithms.set(key='W', value=W.tolist())
-
-        utils.debug_print('End of AR_Random processAnswer')
-        butler.log('Events', {'exp_uid': butler.exp_uid,
-                              'alg':'AR', 'function':'processAnswer',
-                              'left_id':left_id, 'right_id':right_id, 'winner_id':winner_id, 'id':'None',
-                              'timestamp':utils.datetimeNow(),
-                              'waitingforresponse':None,
-                              'stackparametersallqs':None,
-                              'arrlist':None,
-                              'participant':'None',
-                              'msg':'Success'})
+        self.log(butler, [left_id, right_id, winner_id])
         return True
 
     def getModel(self,butler):
-        W = np.array(butler.algorithms.get(key='W'))
-        n = W.shape[0]
-        P = np.zeros((n,n))
-        for i in range(n):
-            for j in range(n):
-                if i==j:
-                    continue
-                if (W[i][j]+W[j][i]==0):
-                    P[i][j] = 0.5
-                else:
-                    P[i][j] = float(W[i][j])/(W[i][j]+W[j][i])
-        scores = np.sum(P, axis=1)/(n-1)
-        butler.log('scores', {'exp_uid':butler.exp_uid,
-                               'scores':scores.tolist()})
-        sortedimages = np.argsort(scores)
-        positions = np.argsort(sortedimages)
-        return positions.tolist()
+        n = butler.algorithms.get(key='n')
+        return range(n)
+
+    def log(self, butler, api_call, query, msg=''):
+        butler.log('ALG-EVALUATION', {'exp_uid': butler.exp_uid, 'timestamp': utils.datetimeNow(),
+                                      'alg_label': butler.alg_label, 'api_call': api_call,
+                                      'query': query, 'msg': msg})
